965cbd41c11392a2865af4298b61b006c12aceaf
coderanac <coder.anac@gmail.com>
2020-06-22 22:26:08 -0300

    index.js

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const form = document.querySelector('#form');
+
+function askAuth(data) {
+  const header = new Headers({
+    'authorization': `Basic ${data}`
+  });
+
+  const init = {
+    method: 'GET',
+    headers: header,
+  };
+
+  const request = new Request('http://localhost:5000/', init);
+
+  fetch(request).then(res => res.text()
+    .then((res) => {
+      document.write(res);
+    }));
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const username = e.target.elements.username.value;
+  const password = e.target.elements.password.value;
+
+  const data = btoa([username, password].join(':'));
+
+  askAuth(data);
+});
