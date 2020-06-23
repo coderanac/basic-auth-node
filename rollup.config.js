6117e4fb6fad3b639136589a0b70c9a46ca3bf1d
coderanac <coder.anac@gmail.com>
2020-06-23 12:43:56 -0300

    config rollup for build js and html

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,24 @@
 import { terser } from "rollup-plugin-terser";
+import spa from 'rollup-plugin-html';
+const html = require('@rollup/plugin-html');
 
 export default {
   input: 'src/index.js',
   plugins: [
     terser(),
+    html(),
+    spa({
+      include: '**/*.html',
+      htmlMinifierOptions: {
+        collapseWhitespace: true,
+        collapseBooleanAttributes: true,
+        conservativeCollapse: true,
+        minifyJS: true
+      }
+    })
   ],
   output: {
     file: 'dist/bundle.js',
-    format: 'umd'
-  }
+    format: 'umd',
+  },
 };
